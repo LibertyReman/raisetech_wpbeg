a4caae863e37a12c2ecfecb86b2b8acb457f71f5
LibertyReman <libertyreman@gmail.com>
2023-04-23 06:54:59 +0900

    目次：テーマチェック

--- functions.php
+++ functions.php
@@ -1,17 +1,21 @@
 <?php
   //テーマサポート
-  add_theme_support( 'menus' );       // メニューの追加
   add_theme_support( 'title-tag' );   // タイトルタグのサポートを許可
   add_theme_support( 'post-thumbnails' ); // アイキャッチをのサポートを追加
-  add_theme_support( ‘automatic-feed-links’ );
+  add_theme_support( 'automatic-feed-links' );
+
+  // テーマのコンテンツエリアのサイズを指定
+  if ( ! isset( $content_width ) ) {
+    $content_width = 960;
+  }
 
   //タイトル出力
   function wpbeg_title( $title ) {
-  if ( is_front_page() && is_home() ) { //トップページなら
-    $title = get_bloginfo( 'name', 'display' );
-  } elseif ( is_singular() ) { //シングルページなら
-    $title = single_post_title( '', false );
-  }
+    if ( is_front_page() && is_home() ) { //トップページなら
+      $title = get_bloginfo( 'name', 'display' );
+    } elseif ( is_singular() ) { //シングルページなら
+      $title = single_post_title( '', false );
+    }
     return $title;
   }
   add_filter( 'pre_get_document_title', 'wpbeg_title' );
@@ -20,7 +24,7 @@
   function wpbeg_script() {
     wp_enqueue_style( 'mplus1p', '//fonts.googleapis.com/earlyaccess/mplus1p.css', array() );
     wp_enqueue_style( 'Sacramento', '//fonts.googleapis.com/css?family=Sacramento&amp;amp;subset=latin-ext', array() );
-    wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.7.0' );
+    wp_enqueue_style( 'font-awesome', '//use.fontawesome.com/releases/v5.6.1/css/all.css', array(), '5.6.1' );
     wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '4.5.0' );
     wp_enqueue_style( 'wpbeg', get_template_directory_uri() . '/css/wpbeg.css', array(), '1.0.0' );
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
@@ -70,7 +74,6 @@
     add_editor_style( get_template_directory_uri() . "/css/editor-style.css" );
   }
   add_action( 'admin_init', 'wpbeg_theme_add_editor_styles' );
-
 
 
 
